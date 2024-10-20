d83a5bc8c825deaf8d692f1968a9d0e5a8fda829
DaSolHan <9511dasole@gmail.com>
2024-10-21 01:27:15 +0900

    second

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,22 @@
+import UserCard from "@/components/UserCard";
+
+const userCard:string[] = ["student", "teacher", "parent", "staff"];
 function AdminPage() {
-    return (
-      <div className=''>AdminPage</div>
-    )
-  }
-  
-  export default AdminPage
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row ">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+        {/* User Card */}
+        <div className="flex gap-4 justify-between">
+          {userCard.map((h) => (
+            <UserCard type={h} />
+          ))}
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">R</div>
+    </div>
+  );
+}
+
+export default AdminPage;
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,9 @@
+function UserCard({ type }: { type: string }) {
+  return <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1">
+    <div className="">
+        
+    </div>
+  </div>;
+}
+
+export default UserCard;
