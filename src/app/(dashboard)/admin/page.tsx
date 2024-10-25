d7efbd358a3de4fb8ace0e025f95b468613db3da
DaSolHan <9511dasole@gmail.com>
2024-10-25 23:55:57 +0900

    work

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,16 +1,34 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
-const userCard:string[] = ["student", "teacher", "parent", "staff"];
+const userCard: string[] = ["student", "teacher", "parent", "staff"];
 function AdminPage() {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row ">
       {/* LEFT */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* User Card */}
         <div className="flex gap-4 justify-between flex-wrap">
-          {userCard.map((h) => (
-            <UserCard type={h} />
+          {userCard.map((h, i) => (
+            <UserCard type={h} n={i} />
           ))}
+        </div>
+        {/* Middle charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count Chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* Attendance Chart */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* bottom charts */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
         </div>
       </div>
       {/* RIGHT */}
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 
-function UserCard({ type }: { type: string }) {
+function UserCard({ type, n }: { type: string, n: number }) {
   return (
     <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px]">
-      <div className="">
-        <span>2024/24</span>
+      <div className="flex justify-between items-center">
+        <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">2024/24</span>
         <Image src="/more.png" alt="" width={20} height={20}/>
       </div>
-      <h1>1,234</h1>
-      <h2>{type}</h2>
+      <h1 className="text-2xl font-semibold my-4">1,234</h1>
+      <h2 className="capitalize text-sm">{type}</h2> 
+      {/* capitalize는 첫 글자만 대문자로 바꿔준다 */}
     </div>
   );
 }
