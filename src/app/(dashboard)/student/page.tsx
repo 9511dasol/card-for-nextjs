5a8c0089a4b4d95175766ff18daef0a7407c998b
DaSolHan <9511dasole@gmail.com>
2024-11-02 01:42:25 +0900

    send

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,19 @@
+import BigCalendar from "@/components/BigCalender";
+
 function StudentPage() {
-    return (
-      <div className=''>StudentPage</div>
-    )
-  }
-  
-  export default StudentPage
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full xl:w-1/3"></div>
+    </div>
+  );
+}
+
+export default StudentPage;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -121,13 +122,23 @@ function Menu() {
     <div className="mt-4 text-sm">
       {menuItems.map((v) => (
         <div className="flex flex-col gap-2" key={v.title}>
-          <span className="hidden lg:block text-gray-400 font-light my-4">{v.title}</span>
-          {v.items.map((val) => (
-            <Link href={val.href} key={val.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
-              <Image src={val.icon} alt="" width={20} height={20} />
-              <span className="hidden lg:block">{val.label}</span>
-            </Link>
-          ))}
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {v.title}
+          </span>
+          {v.items.map((val) => {
+            if (val.visible.includes(role)) {
+              return (
+                <Link
+                  href={val.href}
+                  key={val.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                >
+                  <Image src={val.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{val.label}</span>
+                </Link>
+              );
+            }
+          })}
         </div>
       ))}
     </div>
